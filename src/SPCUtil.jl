1ceed710b17a188ea82a71f9e08c597b131c8848
Sean P. Cornelius <spcornelius@gmail.com>
2022-11-22 11:46:37 -0500

    add label alignment functions for Makie

--- src/SPCUtil.jl
+++ src/SPCUtil.jl
@@ -3,8 +3,13 @@ module SPCUtil
 using Distributed
 using SlurmClusterManager
 
+include("./makie.jl")
 include("./slurm.jl")
 
+# makie.jl
+export align_xlabels!, align_ylabels!
+
+# slurm.jl
 export setup_workers
 
 end # module SPCUtil
