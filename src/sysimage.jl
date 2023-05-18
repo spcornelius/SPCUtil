b4f66e8eddb19c0963a49cb0ec70b34c4654091c
Sean P. Cornelius <spcornelius@gmail.com>
2023-05-18 11:10:17 -0400

    cleanup

--- src/sysimage.jl
+++ src/sysimage.jl
@@ -1,6 +1,9 @@
 import Pkg
 import PackageCompiler
 
+const SYSIMAGE_PREFIX = "JuliaSysimage"
+
+# these sometimes cause segfaults
 const EXCLUDE = Set(["PyCall", "PyPlot"])
 
 function create_sysimage(; kw...)
@@ -13,12 +16,11 @@ function create_sysimage(; kw...)
     pkgs = [Symbol(v.name) for v in values(deps) if 
             v.is_tracking_registry && v.is_direct_dep && v.name ∉ EXCLUDE]
 
-    sysimage_prefix = "JuliaSysimage"
     sysimage_suffix = get(ENV, "JULIA_SYSIMAGE_SUFFIX", "")
     if !isempty(sysimage_suffix)
         sysimage_suffix = "_" * sysimage_suffix
     end
-    sysimage_path = sysimage_prefix * sysimage_suffix * ".so"
+    sysimage_path = SYSIMAGE_PREFIX * sysimage_suffix * ".so"
     sysimage_path = joinpath(project_root, sysimage_path)
     @info "Creating julia sysimage at $sysimage_path..."
     PackageCompiler.create_sysimage(pkgs, sysimage_path=sysimage_path; kw...)
