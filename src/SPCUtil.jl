99d11aeb8c19345b133ccfd51d80e3351b910f6c
Sean P. Cornelius <spcornelius@gmail.com>
2023-06-22 09:37:47 -0500

    add is_logging

--- src/SPCUtil.jl
+++ src/SPCUtil.jl
@@ -4,9 +4,13 @@ using Distributed
 using Distributed: SSHManager, LocalManager
 using Makie
 
+include("./general.jl")
 include("./makie.jl")
 include("./distributed.jl")
 include("./sysimage.jl")
+
+# general.jl
+export is_logging
 
 # makie.jl
 export align_xlabels!, align_ylabels!
